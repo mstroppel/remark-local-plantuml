d45f0896dec5bf030021c556a50de8a49e31c75e
Marco Stroppel <marco.stroppel@gmail.com>
2023-08-20 16:15:18 +0200

    wip

--- index.js
+++ index.js
@@ -1,34 +1,39 @@
-const visit = require("unist-util-visit");
-const plantumlEncoder = require("plantuml-encoder");
-
-const DEFAULT_OPTIONS = {
-  baseUrl: "https://www.plantuml.com/plantuml/png"
-};
+//const plantuml = require("node-plantuml");
+//const plantumlGenerator = plantuml.generate({ format: "svg" });
 
 /**
  * Plugin for remark-js
  *
  * See details about plugin API:
  * https://github.com/unifiedjs/unified#plugin
- *
- * You can specify the endpoint of PlantUML with the option 'baseUrl'
- *
- * @param {Object} pluginOptions Remark plugin options.
  */
-function remarkSimplePlantumlPlugin(pluginOptions) {
-  const options = { ...DEFAULT_OPTIONS, ...pluginOptions };
-
-  return function transformer(syntaxTree) {
-    visit(syntaxTree, "code", node => {
-      let { lang, value, meta } = node;
+function remarkSimplePlantumlPlugin() {
+  return async syntaxTree => {
+    for (let i = 0; i < syntaxTree.children.length; i++) {
+      let node = syntaxTree.children[i];
+      let { lang, value, alt } = node;
       if (!lang || !value || lang !== "plantuml") return;
 
-      node.type = "image";
-      node.url = `${options.baseUrl.replace(/\/$/, "")}/${plantumlEncoder.encode(value)}`;
-      node.alt = meta;
-      node.meta = undefined;
-    });
-    return syntaxTree;
+      /*
+      let svgString = "";
+      plantumlGenerator.in = value;
+
+      await new Promise(resolve => {
+        plantumlGenerator.out.on("data", data => {
+          svgString += data.toString("utf8");
+        });
+
+        plantumlGenerator.out.on("end", () => {
+          resolve();
+        });
+      });
+      */
+      let svgString = "heodhandao";
+      node.type = "html";
+      node.value = `<div class="plantuml-diagram">${svgString}</div>`;
+      node.alt = alt;
+      node.meat = undefined;
+    }
   };
 }
 
