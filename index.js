4a0ecf0f3963b2f959cb2fa57d4b19500f33b74b
Simon Eßlinger <simon.esslinger@tapio.one>
2026-01-07 13:57:58 +0100

    feat: limit parallel processing to 4 nodes at once

--- index.js
+++ index.js
@@ -2,6 +2,45 @@ import visit from "unist-util-visit";
 import plantuml from "node-plantuml-back";
 
 /**
+ * Utility function to chunk an array into smaller arrays of specified size
+ * @param {Array} array - The array to chunk
+ * @param {number} size - The size of each chunk
+ * @returns {Array[]} - Array of chunks
+ */
+function chunk(array, size) {
+  const chunks = [];
+  for (let i = 0; i < array.length; i += size) {
+    chunks.push(array.slice(i, i + size));
+  }
+  return chunks;
+}
+
+/**
+ * Process a single PlantUML node and convert it to HTML
+ * @param {Object} node - The PlantUML code node
+ * @returns {Promise} - Promise that resolves when the node is processed
+ */
+function processPlantUMLNode(node) {
+  let { value, alt } = node;
+  let svgString = "";
+  const plantumlGenerator = plantuml.generate(value, { format: "svg" });
+
+  return new Promise((resolve) => {
+    plantumlGenerator.out.on("data", (data) => {
+      svgString += data.toString("utf8");
+    });
+
+    plantumlGenerator.out.on("end", () => {
+      node.type = "html";
+      node.value = `<div class="plantuml-diagram">${svgString}</div>`;
+      node.alt = alt;
+      node.meat = undefined;
+      resolve();
+    });
+  });
+}
+
+/**
  * Plugin for remark-js
  *
  * See details about plugin API:
@@ -17,29 +56,12 @@ function remarkLocalPlantumlPlugin() {
       }
     });
 
-    let promises = [];
-    for (const node of nodes) {
-      let { value, alt } = node;
-      let svgString = "";
-      const plantumlGenerator = plantuml.generate(value, { format: "svg" });
+    const nodeChunks = chunk(nodes, 4);
 
-      let promise = new Promise((resolve) => {
-        plantumlGenerator.out.on("data", (data) => {
-          svgString += data.toString("utf8");
-        });
-
-        plantumlGenerator.out.on("end", () => {
-          node.type = "html";
-          node.value = `<div class="plantuml-diagram">${svgString}</div>`;
-          node.alt = alt;
-          node.meat = undefined;
-          resolve();
-        });
-      });
-      promises.push(promise);
+    for (const nodeChunk of nodeChunks) {
+      const chunkPromises = nodeChunk.map(node => processPlantUMLNode(node));
+      await Promise.all(chunkPromises);
     }
-
-    await Promise.all(promises);
   };
 }
 
